b526a66c1e08b77eec78e91516b0868b2498e291
Adán <atoscano@emergya.com>
2020-08-05 17:45:35 +0200

    Test canvas

--- packages/screen/src/App.js
+++ packages/screen/src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
 import './App.css';
 
+// Scaling Constants for Canvas
+const canvasWidth = window.innerWidth * .5;
+const canvasHeight = window.innerHeight * .5;
+
+function drawCircles(ctx){
+    console.log("attempting to draw circles")
+    const centerX = canvasWidth / 2;
+    const centerY = canvasHeight / 2;
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, 100, 0, 2 * Math.PI, false);
+    ctx.closePath();
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, 50, 0, 2 * Math.PI, false);
+    ctx.closePath();
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, 20, 0, 2 * Math.PI, false);
+    ctx.closePath();
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.restore();  
+};
+
 function App() {
+  const canvasRef = useRef(null);
+
+  useEffect(()=>{
+      const canvasObj = canvasRef.current;
+      const ctx = canvasObj.getContext('2d');
+      // clear the canvas area before rendering the coordinates held in state
+      ctx.clearRect( 0,0, canvasWidth, canvasHeight );
+      drawCircles(ctx)
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas 
+        className="App-canvas"
+        ref={canvasRef}
+        width={canvasWidth}
+        height={canvasHeight} />
     </div>
   );
 }
