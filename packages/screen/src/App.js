c6c14dae11ddfefce18e7e14745cf67ccc64a2db
Adán Toscano <adantoscano@gmail.com>
2020-09-08 18:58:44 +0000

    Add semantic components

--- packages/screen/src/App.js
+++ packages/screen/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'semantic-ui-react'
 import Canvas from './components/Canvas';
 import './App.css';
 
 function App() {
+  const [isScreen, setScreen] = useState(false);
+  const [isDevice, setDevice] = useState(false);
+
+  const handleScreenButton = () => setScreen(true);
+  const handleDeviceButton = () => setDevice(true);
+
   return (
     <div className="App">
-      <Canvas />
-    </div>
+      {
+        (!isScreen && !isDevice) && <div> <Button onClick={handleScreenButton}>Screen</Button><Button onClick={handleDeviceButton}>Device</Button> </div>
+      }
+      { isScreen && <Canvas /> }
+      { isDevice && 'Soy un móvil' }
+    </div >
   );
 }
 
