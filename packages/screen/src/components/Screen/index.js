bfec80d6bf87711fd7595b1c0c871935e0d9177f
Adán Toscano <adantoscano@gmail.com>
2020-10-04 23:07:37 +0000

    set canvas at screen

--- packages/screen/src/components/Screen/index.js
+++ packages/screen/src/components/Screen/index.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import Peer from 'simple-peer';
 import { Button, Form, TextArea } from 'semantic-ui-react';
+import { Stage, Layer, Line, Text } from 'react-konva';
 import QRCode from 'qrcode.react';
 import axios from 'axios';
 
 const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000'
 
 class Screen extends React.Component {
-  constructor () {
+  constructor() {
     super()
     this.state = {
       answer: '',
       offer: '',
       dataReceived: '',
       dataToSend: '',
-      isConnected: false
+      isConnected: false,
+      points: []
     }
 
     this.peer = new Peer({
       initiator: true,
       trickle: false
     });
+
+    this.canvasHeight = window.innerWidth
+    this.canvasWidth = window.innerWidth
   }
 
   componentDidMount() {
@@ -36,12 +41,32 @@ class Screen extends React.Component {
 
     this.peer.on('connect', () => {
       console.log('CONNECT')
-      this.setState({isConnected: true})
+      this.setState({ isConnected: true })
     })
 
     this.peer.on('data', dataReceived => {
       console.log(`dataReceived: ${dataReceived}`)
-      this.setState({dataReceived})
+      this.setState({ dataReceived })
+      const [gamma, beta] = dataReceived;
+      const x = parseFloat(gamma).toPrecision(5);
+      const y = parseFloat(beta).toPrecision(5);
+      const canvasCenterX = this.canvasHeight/2;
+      const canvasCenterY = this.canvasWidth/2;
+      const canvasX = Math.abs(x) <= 90 ? parseInt(x*(this.canvasHeight/180)+(canvasCenterX)) : null;
+      const canvasY = Math.abs(y) <= 90 ? parseInt(y*(this.canvasWidth/180)+(canvasCenterY)) : null;
+      console.log(`canvasHeight: ${this.canvasHeight}`);
+      console.log(`canvasWisth: ${this.canvasWidth}`);
+      console.log(`canvasX: ${canvasX}`);
+      console.log(`canvasY: ${canvasY}`);
+      if (canvasX && canvasY){
+        this.setState({
+          gData: {
+            x,
+            y
+          },
+          points: [...this.state.points, parseInt(canvasX), parseInt(canvasY)]
+        })
+      }
     })
   }
 
@@ -50,7 +75,7 @@ class Screen extends React.Component {
     const res = await axios(this.state.offer);
     res.data && res.data.answer ?
       this.peer.signal(res.data.answer) :
-      setTimeout(() => this.getServerAnswer(), 1000); 
+      setTimeout(() => this.getServerAnswer(), 1000);
   }
 
   handleSubmitData = () => {
@@ -62,15 +87,30 @@ class Screen extends React.Component {
     return (
       <div>
         <div>
-        {this.state.isConnected ? 'CONNECTED' : 'OFFLINE'}
+          {this.state.isConnected ? 'CONNECTED' : 'OFFLINE'}
         </div>
+        <Stage
+          height={this.canvasHeight}
+          width={this.canvasWidth}
+        >
+          <Layer>
+            <Text text="Just start drawing" x={5} y={30} />
+            <Line
+              points={this.state.points}
+              stroke="#df4b26"
+              strokeWidth={5}
+              tension={0.5}
+              lineCap="round"
+            />
+          </Layer>
+        </Stage>
         <div>
-        {this.state.offer}
+          {this.state.offer}
         </div>
-        {this.state.offer && <QRCode value= {this.state.offer}/>}
+        {this.state.offer && <QRCode value={this.state.offer} />}
         <Form>
-          <TextArea placeholder='Send data to peer' onChange={e=>this.setState({dataToSend: e.target.value})} />
-          <Button onClick={this.handleSubmitData}/>
+          <TextArea placeholder='Send data to peer' onChange={e => this.setState({ dataToSend: e.target.value })} />
+          <Button onClick={this.handleSubmitData} />
         </Form>
         {this.state.dataReceived}
       </div>
