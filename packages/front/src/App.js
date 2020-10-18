4f17a16f9010a62ec2654ff59428a760051f7e17
Adan Toscano <adantoscano@gmail.com>
2020-10-19 01:38:00 +0200

    chore(front): Join screen and device

--- packages/front/src/App.js
+++ packages/front/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Peer from 'simple-peer';
 import { Button, Input } from 'semantic-ui-react';
 import QrReader from 'react-qr-reader'
+import QRCode from 'qrcode.react';
 import axios from 'axios';
 
-import Radar from '../Radar';
+import Radar from './components/Radar';
 
+const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000'
 
-class Device extends React.Component {
+class App extends React.Component {
   constructor() {
     super()
     this.state = {
@@ -52,10 +54,6 @@ class Device extends React.Component {
 
   componentDidMount() {
     window.addEventListener('deviceorientation', this.getPointer, true);
-  }
-
-  handleSubmitData = () => {
-    this.peer.send(this.state.dataToSend);
   }
 
   handleScan = async data => {
@@ -114,6 +112,51 @@ class Device extends React.Component {
     this.setState({ showQRScanner: true });
   }
 
+  handleConnectDevice = () => {
+    this.peer = new Peer({
+      initiator: true,
+      trickle: false
+    });
+
+    this.peer.on('error', err => console.log('error', err))
+
+    this.peer.on('signal', async data => {
+      console.log('SIGNAL', JSON.stringify(data))
+      const res = await axios.post(apiUrl, data);
+      console.log('SIGNAL', JSON.stringify(res));
+      this.setState({ offer: res.data });
+      this.getServerAnswer();
+    })
+
+    this.peer.on('connect', () => {
+      console.log('CONNECT')
+      this.setState({ isConnected: true })
+    })
+
+    this.peer.on('data', dataReceived => {
+      console.log(`dataReceived: ${dataReceived}`)
+      this.setState({ dataReceived })
+      const [gamma, beta] = dataReceived.toString().split(',');
+      const { x, y } = this.parseDegreesToCanvas(gamma, beta);
+      if (x && y) {
+        this.setState({
+          orientation: {
+            x,
+            y
+          }
+        })
+      }
+    })
+  }
+
+  getServerAnswer = async () => {
+    console.log('Getting');
+    const res = await axios(this.state.offer);
+    res.data && res.data.answer ?
+      this.peer.signal(res.data.answer) :
+      setTimeout(() => this.getServerAnswer(), 1000);
+  }
+
   startMeasure = event => {
     const { beta, gamma } = event;
     const { x, y } = this.parseDegreesToCanvas(gamma, beta);
@@ -142,16 +185,18 @@ class Device extends React.Component {
           onError={this.handleError}
           onScan={this.handleScan}
           style={{ width: '100%' }}
-        /> }
+          /> }
         <Radar
           x={this.state.orientation.x}
           y={this.state.orientation.y}
           points={this.state.points}
           size={this.radarSize} />
+        {this.state.offer && <QRCode value={this.state.offer} includeMargin/>}
         <Button onClick={this.handleStartMeasure}>Start measure</Button>
         <Button onClick={this.handleStopMeasure}>Stop measure</Button>
         <Button onClick={() => this.setState({ points: [] })}>Clear measure</Button>
         <Button onClick={this.handleShowQRScanner}>Connect with screen</Button>
+        <Button onClick={this.handleConnectDevice}>Connect with device</Button>
         <Input placeholder='Time in seconds' onChange={e => this.setState({ timerCount: e.target.value })} />
         {this.state.timerCount} <br />
         {this.state.orientation.x} <br />
@@ -162,4 +207,4 @@ class Device extends React.Component {
   }
 }
 
-export default Device;
+export default App;
