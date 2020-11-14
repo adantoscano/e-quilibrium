cf95fa82a0f3e18ccacdaba2b992c4a30a261340
Adán <atoscano@emergya.com>
2020-11-14 15:29:03 +0100

    feat(screen): render qr at start and clean unused code

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Peer from 'simple-peer';
 import { Button, Input } from 'semantic-ui-react';
-import QrReader from 'react-qr-reader'
 import QRCode from 'qrcode.react';
 import axios from 'axios';
 
@@ -57,57 +56,8 @@ class Screen extends React.Component {
   }
 
   componentDidMount() {
-    window.addEventListener('deviceorientation', this.getPointer, true);
-  }
-
-  componentDidUpdate() {
-    if(this.state.isConnectedToHUD) {
-      this.peer.send(JSON.stringify({
-        orientation: this.state.orientation,
-        points: this.state.points,
-        timerCount: this.state.timerCount
-      }))
-    }
-  }
-
-  handleScan = async data => {
-    if (!this.state.qrData && data) {
-      this.setState({
-        qrData: data,
-        showQRScanner: false
-      })
-      const res = await axios(data);
-      this.peer.signal(res.data.offer);
-    }
-  }
-
-  handleStartMeasure = () => {
-    if (this.state.isConnectedToDevice) {
-      this.peer.send(JSON.stringify({
-        startMeasure: true
-      }))
-    }
-    if (parseInt(this.state.timerCount) > 0) {
-      const initialTimer = this.state.timerCount;
-      const interval = setInterval(() => {
-        this.setState({ timerCount: this.state.timerCount - 1 })
-        if (this.state.timerCount <= 0) {
-          this.handleStopMeasure();
-          this.setState({ timerCount: initialTimer })
-          clearInterval(interval);
-        }
-      }, 1000);
-    }
-    window.addEventListener('deviceorientation', this.startMeasure, true);
-  }
-
-  handleStopMeasure = () => {
-    window.removeEventListener('deviceorientation', this.startMeasure, true);
-    this.setState({ startMeasure: false })
-  }
-
-  handleShowQRScanner = () => {
     this.peer = new Peer({
+      initiator: true,
       trickle: false
     });
 
@@ -115,23 +65,30 @@ class Screen extends React.Component {
 
     this.peer.on('signal', async data => {
       console.log('SIGNAL', JSON.stringify(data))
-      const res = await axios.post(this.state.qrData, data);
-      this.setState({ answer: res.data })
+      const res = await axios.post(apiUrl, data);
+      console.log('SIGNAL', JSON.stringify(res));
+      this.setState({ offer: res.data });
+      this.getServerAnswer();
     })
 
     this.peer.on('connect', () => {
-      console.log('CONNECT');
-      this.setState({isConnectedToHUD: true});
+      console.log('CONNECT')
+      this.setState({ isConnectedToDevice: true })
     })
 
-    this.peer.on('data', data => {
-      const dataJson = JSON.parse(data.toString());
-      if(dataJson.startMeasure) {
-        this.handleStartMeasure();
-      }
-    })
+    this.peer.on('data', dataReceived => {
+      this.setState(JSON.parse(dataReceived.toString()));
+    });
+  }
 
-    this.setState({ showQRScanner: true });
+  handleStartMeasure = () => {
+    this.peer.send(JSON.stringify({
+      startMeasure: true
+    }));
+  }
+
+  handleStopMeasure = () => {
+    this.setState({ startMeasure: false })
   }
 
   handleConnectToDevice = () => {
@@ -165,61 +122,35 @@ class Screen extends React.Component {
     const res = await axios(this.state.offer);
     res.data && res.data.answer ?
       this.peer.signal(res.data.answer) :
-      setTimeout(() => this.getServerAnswer(), 1000);
-  }
-
-  startMeasure = event => {
-    const { beta, gamma } = event;
-    const x = parseFloat(gamma).toPrecision(5);
-    const y = parseFloat(beta).toPrecision(5);
-    if (x && y) {
-      this.setState({
-        points: [...this.state.points, x, y]
-      })
-    }
-  }
-
-  handleGetMaxTilt = () => {
-    this.setState({ maxTilt: 0 });
-    window.addEventListener('deviceorientation', this.getMaxTilt, true);
-    setTimeout(() => window.removeEventListener('deviceorientation', this.getMaxTilt, true), 5000);
-  }
-
-  getMaxTilt = event => {
-    const { beta, gamma } = event;
-    const x = parseFloat(gamma).toPrecision(5);
-    const y = parseFloat(beta).toPrecision(5);
-    this.setState({ maxTilt: Math.max(this.state.maxTilt, Math.abs(x), Math.abs(y)) });
+      setTimeout(() => this.getServerAnswer(), 5000);
   }
 
   render() {
     return (
       <div>
-        { this.state.showQRScanner && <QrReader
-          delay={300}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          style={{ width: '100%' }}
-          /> }
-        <Radar
-          pointerX={this.state.orientation.x}
-          pointerY={this.state.orientation.y}
-          points={this.state.points}
-          size={this.radarSize}
-          maxTilt={this.state.maxTilt} />
-        {this.state.offer && <QRCode value={this.state.offer} includeMargin/>}
-        <Button onClick={this.handleStartMeasure}>Start measure</Button>
-        <Button onClick={this.handleStopMeasure}>Stop measure</Button>
-        <Button onClick={this.handleGetMaxTilt}>Get max tilt</Button>
-        <Button onClick={() => this.setState({ points: [] })}>Clear measure</Button>
-        <Input placeholder='Time in seconds' onChange={e => this.setState({ timerCount: e.target.value })} />
-        <Button onClick={this.handleShowQRScanner}>Connect with screen</Button>
-        <Button onClick={this.handleConnectToDevice}>Connect with device</Button>
-        {this.state.timerCount} <br />
-        {this.state.orientation.x} <br />
-        {this.state.orientation.y} <br />
-        {this.state.qrData} <br />
-        {this.state.maxTilt} <br />
+        {!this.state.offer && <div> Generating data to sync... </div>}
+        {this.state.offer && !this.state.isConnectedToDevice && <QRCode value={this.state.offer} includeMargin/>}
+        {this.state.isConnectedToDevice &&
+          <div>
+          <Radar
+            pointerX={this.state.orientation.x}
+            pointerY={this.state.orientation.y}
+            points={this.state.points}
+            size={this.radarSize}
+            maxTilt={this.state.maxTilt} />
+          <Button onClick={this.handleStartMeasure}>Start measure</Button>
+          <Button onClick={this.handleStopMeasure}>Stop measure</Button>
+          <Button onClick={this.handleGetMaxTilt}>Get max tilt</Button>
+          <Button onClick={() => this.setState({ points: [] })}>Clear measure</Button>
+          <Input placeholder='Time in seconds' onChange={e => this.setState({ timerCount: e.target.value })} />
+          <Button onClick={this.handleShowQRScanner}>Connect with screen</Button>
+          <Button onClick={this.handleConnectToDevice}>Connect with device</Button>
+          {this.state.timerCount} <br />
+          {this.state.orientation.x} <br />
+          {this.state.orientation.y} <br />
+          {this.state.qrData} <br />
+          {this.state.maxTilt} <br />
+        </div>}
       </div>
     );
   }
