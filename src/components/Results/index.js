6d1a8ee882bb3366d3090d9fc56f0be940ffe41c
Adán Toscano <atoscano@emergya.com>
2020-11-26 01:45:33 +0100

    feat(Results): show zone and quadrant percentage

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -27,7 +27,7 @@ function getOveralStabilityDesviation(points) {
     }, []);
   const magnitudeSum = magnitudeArr.reduce((acc, value) => acc + value, 0);
   const meanDeflection = magnitudeSum / magnitudeArr.length;
-  const desviationSum = magnitudeArr.reduce((acc, value) => acc + Math.sqrt(Math.pow(value - meanDeflection, 2)));
+  const desviationSum = magnitudeArr.reduce((acc, value) => acc + Math.sqrt(Math.pow(value - meanDeflection, 2)), 0);
   return parseFloat(desviationSum / magnitudeArr.length).toPrecision(5);
 }
 
@@ -59,7 +59,44 @@ function getLateralStabilityDesviation(points) {
   return parseFloat(desviationSum/(points.length/2)).toPrecision(5) // points.length/2 = number of samples, sample = x1,y1
 }
 
+function getZonePercentage(points, maxTilt) {
+  const pointsPerZone = points.reduce((acc, value, index, arr) => {
+    if (!(index % 2)) return acc;
+    const magnitude = Math.sqrt(Math.pow(arr[index-1], 2) + Math.pow(arr[index], 2));
+    if (magnitude <= maxTilt/4) return Object.assign(acc, { a: acc.a + 1 });
+    if (magnitude <= (maxTilt/4)*2) return Object.assign(acc, { b: acc.b + 1 });
+    if (magnitude <= (maxTilt/4)*3) return Object.assign(acc, { c: acc.c + 1 });
+    return Object.assign(acc, { d: acc.d + 1 });
+  }, {a:0,b:0,c:0,d:0});
+
+  return {
+    a: parseFloat((pointsPerZone.a*100)/(points.length/2)).toPrecision(3),
+    b: parseFloat((pointsPerZone.b*100)/(points.length/2)).toPrecision(3),
+    c: parseFloat((pointsPerZone.c*100)/(points.length/2)).toPrecision(3),
+    d: parseFloat((pointsPerZone.d*100)/(points.length/2)).toPrecision(3)
+  }
+}
+
+function getQuadrantPercentage(points) {
+  const pointsPerQuadrant = points.reduce((acc, value, index, arr) => {
+    if (!(index % 2)) return acc;
+    if (arr[index-1] < 0 && arr[index] < 0) return Object.assign(acc, { a: acc.a + 1 });
+    if (arr[index-1] > 0 && arr[index] < 0) return Object.assign(acc, { b: acc.b + 1 });
+    if (arr[index-1] < 0 && arr[index] > 0) return Object.assign(acc, { c: acc.c + 1 });
+    return Object.assign(acc, { d: acc.d + 1 });
+  }, {a:0,b:0,c:0,d:0});
+
+  return {
+    a: parseFloat((pointsPerQuadrant.a*100)/(points.length/2)).toPrecision(3),
+    b: parseFloat((pointsPerQuadrant.b*100)/(points.length/2)).toPrecision(3),
+    c: parseFloat((pointsPerQuadrant.c*100)/(points.length/2)).toPrecision(3),
+    d: parseFloat((pointsPerQuadrant.d*100)/(points.length/2)).toPrecision(3)
+  }
+}
+
 function Results({points, size, maxTilt, close}) {
+  const zonePercentage = getZonePercentage(points, maxTilt);
+  const quadrantPercentage = getQuadrantPercentage(points);
   return (
     <Modal
       onClose={() => close()}
@@ -74,13 +111,17 @@ function Results({points, size, maxTilt, close}) {
           size={size}
           maxTilt={maxTilt}
           />
+      </Modal.Content>
+      <Modal.Description>
         <p>Overal Stability Index: {getOveralStabilityIndex(points)}</p>
         <p>Overal Stability Desviation: {getOveralStabilityDesviation(points)}</p>
         <p>Antero/Posterior Stability Index: {getPosteriorStabilityIndex(points)}</p>
         <p>Antero/Posterior Stability Desviation: {getPosteriorStabilityDesviation(points)}</p>
         <p>Medial/Lateral Stability Index: {getLateralStabilityIndex(points)}</p>
         <p>Medial/Lateral Stability Desviation: {getLateralStabilityDesviation(points)}</p>
-      </Modal.Content>
+        <p>Zone Percentage: A{zonePercentage.a}% B{zonePercentage.b}% C{zonePercentage.c}% D{zonePercentage.d}%</p>
+        <p>Zone Percentage: I{quadrantPercentage.a}% II{quadrantPercentage.b}% III{quadrantPercentage.c}% IV{quadrantPercentage.d}%</p>
+      </Modal.Description>
       <Modal.Actions>
         <Button color='black' onClick={() => close()}>
           Nope
